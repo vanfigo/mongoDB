cba2b3af040d86f87e6e3c700b9e66253e7f1eb1
vanfigo <rodrigo.loyola@live.com>
2017-09-23 16:12:47 -0500

    Adding update many

--- src/main/java/com/mongodb/M101J/crud/UpdateTest.java
+++ src/main/java/com/mongodb/M101J/crud/UpdateTest.java
@@ -1,5 +1,9 @@
 package com.mongodb.M101J.crud;
 
+import static com.mongodb.M101J.Helpers.printJson;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Updates.*;
+
 import java.util.ArrayList;
 
 import org.bson.Document;
@@ -7,8 +11,6 @@ import org.bson.Document;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
-import static com.mongodb.M101J.Helpers.printJson;
 
 public class UpdateTest {
 
@@ -25,6 +27,13 @@ public class UpdateTest {
 					.append("x", i)
 					.append("y", true));
 		}
+		
+		//collection.replaceOne(Filters.eq("x", 5), new Document("x", 20).append("updated", true));
+		//collection.updateOne(eq("x", 5), new Document("$set", new Document("x", 20).append("updated", true)));
+		//collection.updateOne(eq("x", 5), set("x", 20));
+		//collection.updateOne(eq("x", 5), combine(set("x", 20), set("updated", true)));
+		//collection.updateOne(eq("_id", 9), combine(set("x", 20), set("updated", true)), new UpdateOptions().upsert(true));
+		collection.updateMany(gte("x", 5), inc("x", 1));
 		
 		for(Document document : collection.find().into(new ArrayList<Document>())) {
 			printJson(document);
